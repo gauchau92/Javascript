ce02f5ebcaeaf002afeadb7a2ae826fa88b717e2
Gaurav Chaudhary <gauchau92@gmail.com>
2025-10-03 15:05:49 +0530

    string practice

--- Basics/string.js
+++ Basics/string.js
@@ -1,4 +1,4 @@
-let boy1 = `Gaurav`;
+let boy1 = `Gaurav Chaudhary`;
 let boy2 = `Ram`;
 let boy3 = `Hanuman`;
 
@@ -7,13 +7,6 @@ console.log(`${boy1} is friend of ${boy2} and ${boy3}`); //! String interpolatio
 
 console.log(boy1.length);
 console.log(boy1[0]);
-
-let idea = 'Gaurav\'s idea'; //! Escape sequence
-console.log(idea);
-console.log(idea.length);
-console.log(idea[5]);
-console.log(idea[6]);
-
 
 // Escape Sequence	Meaning	Example
 // \'	Single quote	'It\'s fine' → It's fine
@@ -24,3 +17,30 @@ console.log(idea[6]);
 // \r	Carriage return	"Hello\rWorld" (overwrites line start in some contexts)
 // \b	Backspace	"abc\b" → may remove c depending on environment
 // \f	Form feed (rarely used)	"Hello\fWorld"
+
+console.log(boy1.toUpperCase());
+console.log(boy1.toLowerCase());
+console.log(boy1.slice(2, 4)); //! slice string between given start and END-1 index; index start from 0.
+console.log(boy1.slice(2)); //! slice string from given index til end
+
+console.log(boy1.replace(`Gaurav`, `Vedagya`));
+console.log(boy1.concat(boy2, boy3, `OM`));
+
+let str = "hello";
+str = "yippiieee";
+
+console.log(str);
+str[0] = "Z"; // attempt to change first character
+console.log(str); // still "hello"
+
+//! printing string using for loop
+
+for (i = 0; i < boy1.length; i++) {
+  console.log(boy1[i]);
+}
+
+let result = ``;
+for (i = 0; i < boy1.length; i++) {
+  result += boy1[i];
+}
+console.log(result);
