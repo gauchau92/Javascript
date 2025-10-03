18fa6ab40c2f8b47733efa2e46f3f5cf4434f300
Gaurav Chaudhary <gauchau92@gmail.com>
2025-10-03 16:49:26 +0530

    NaN update

--- Basics/if else-elif.js
+++ Basics/if else-elif.js
@@ -6,10 +6,12 @@ console.log("type of a, when its taken by prompt is:", typeof a);
 a = Number.parseInt(a);
 console.log("type of a, when its converted to number is:", typeof a);
 
-if (a < 0 || a > 130) {
+if (Number.isNaN(a)) {
+  console.log("Please enter a valid numeric age");
+} else if (a < 0 || a > 130) {
   console.log("In-valid age");
 } else if (a >= 0 && a < 18) {
   console.log("Driving not allowed");
 } else {
-  console.log("you can Drive");
+  console.log("You can Drive");
 }
