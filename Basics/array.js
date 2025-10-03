bb609ef0ad8ce4b65089b01faf87fc2dc11a5e59
Gaurav Chaudhary <gauchau92@gmail.com>
2025-10-03 22:39:16 +0530

    array

--- Basics/array.js
+++ Basics/array.js
@@ -26,3 +26,34 @@ console.log(arr);
 let unsft = arr.unshift("First"); //! Adds first item & return length
 console.log(unsft);
 console.log(arr);
+
+delete arr[0]; //! deletes item from array but length doesn't change
+console.log(arr);
+
+let arr1 = [1, 2, 3];
+let arr2 = [6, 5, 4];
+let arr3 = [8, 9, 7];
+
+console.log(arr1.concat(arr2, arr3)); //! adds arrays
+console.log(arr1.concat(arr2, arr3).sort()); //! sort arrays
+
+//! sort func modifies the main arrays & sorts in alphanyumeric order so to overcome that we need to give compare function to actual sort in asending order.
+
+let arr4 = arr1.concat(arr2, arr3);
+console.log(arr4.sort((a, b) => a - b));
+
+//? or else create compare fun outside and call that
+
+const compare = (a, b) => a - b;
+
+console.log(arr4.sort(compare));
+
+console.log(arr4.reverse()); //! reverse the array
+
+let arr5 = [656, 454, 564, 659, 979, 323, 54515];
+arr5.splice(2, 3, 848, 26, 31315, 10110, 7965212); //! splice:- removes and add items to array & returns deleted items. Start index-2, deleted-3, added remianing items at that place
+console.log(arr5);
+console.log(arr5.length);
+
+//console.log(arr5.slice(3)); //! returns new array of sliced piece
+console.log(arr5.slice(3, 7)); //! returns new array of sliced piece start 2 to 6 (7 not included)
